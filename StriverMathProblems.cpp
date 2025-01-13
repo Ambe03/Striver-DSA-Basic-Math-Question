89ad1a20cd751471c6aeb8f3201aad62fad59aac
Ambe03 <ambika032003@gmail.com>
2025-01-13 17:13:38 +0545

    count digit

---
+++ StriverMathProblems.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+//1. count digits: Find the number of digits of ‘n’ that evenly divide ‘n’.
+		
+/*	int num =n;
+	int cnt=0;
+	while(num>0)
+	{
+		int digit = num%10;
+        if (digit != 0) 
+		{
+            // cnt+= (n%digit ==0) ;
+			if(n%digit==0)
+			{
+				cnt++;
+			}
+        }
+		num= num/10;
+    }
+	return cnt;
+*/cd BAsic
