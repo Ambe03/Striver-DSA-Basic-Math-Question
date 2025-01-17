ce4c6a8fc8558c18c6d6813f7332ff3570bd8c5b
Ambe03 <ambika032003@gmail.com>
2025-01-17 22:27:41 +0545

    Armstrong and divisors problem

---
+++ Armstrong.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include<vector>
+#include<math.h>
+using namespace std;
+
+bool armstrong(int n)
+{
+	int num = n;
+	int sum =0;
+	int size =0;
+	int count=0;
+	
+	//APPROACH IST
+	    /*
+			vector<int> v;
+			while(n!=0)
+			{
+				int digit = num%10;
+				num = num/10;
+				v.push_back(digit);
+				size = v.size();
+				sum = sum + pow(digit, size);
+			}
+			
+			if(sum == n)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		*/
+	//APPROACH IInd
+	while(num!=0)
+	{
+		num/=10;
+		count++;
+	}
+	while(n!=0)
+	{
+		int digit = n%10;
+		sum = sum+ pow(digit, count);
+		n= n/10;
+	}
+	return sum == n;
+	
+}
+int main()
+{
+	int n;
+	cin>>n;
+	if(armstrong(n))
+	{
+		cout<<"true";
+	}
+	else
+	{
+		cout<<"False";
+	}
+}
--- prime number.cpp
+++ prime number.cpp
@@ -9,17 +9,19 @@ bool isPrime(int n)
 	{
         return false; 
     }
+    if(n==2)
+    {
+    	return true;
+	}
 	for(int i=2; i<=sqrt(n);i++)
 	{
 		if(n%i == 0)
 		{
 			return false;
 		}
-			else
-		{
-			return true;
-		}
+		
 	}
+	return true;
 
 	
 }
