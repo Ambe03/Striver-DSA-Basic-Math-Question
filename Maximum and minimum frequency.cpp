fff6b50a741db8e1d4c02f5e81d339b7f178cddc
Ambe03 <ambika032003@gmail.com>
2025-01-19 14:33:01 +0545

    Maximum and Minimum frequency of occuring of an element in array

---
+++ Maximum and minimum frequency.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+void count(int arr[], int n)
+{
+	int hash[21] = {0};
+	for(int i=0; i<n; i++)
+	{
+		hash[arr[i]] += 1;
+	}
+	
+	
+	int max = 0;
+	int min = INT_MAX;
+	int maxele;
+	int minele;
+	for(int i=0; i<n; i++)
+	{
+		if(hash[i] >max)
+		{
+			max = hash[i];
+			maxele =i;
+		}
+		if(hash[i] >0 && hash[i]<min)
+		{
+			
+			min = hash[i];
+			minele = i;
+		}		
+	}
+	cout << "Max frequency: " << max<<endl;
+	cout<<"max item"<<maxele<< endl;
+    cout << "Min frequency: " << min <<endl;
+	cout<<"min item"<<minele<< endl;
+	
+	
+}
+
+int main()
+{
+	int arr[10] = {3,3,3,3,4,4,2,2,2,6};
+	int n = 10;
+
+	
+	count(arr, n);
+}
